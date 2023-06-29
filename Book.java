bd068099a5dd15a7ac53d85250b316fecd5cb797
Patryk241194 <128822106+Patryk241194@users.noreply.github.com>
2023-06-29 18:12:15 +0200

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    private String name;
+    private double price;
+    private String category;
+
+    public Book(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.category = BOOKS_AND_COMICS;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getCategory() {
+        return category;
+    }
+}
