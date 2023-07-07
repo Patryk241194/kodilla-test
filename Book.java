b2e819330a111e1e5153d58c491c181bc37b5725
Patryk241194 <128822106+Patryk241194@users.noreply.github.com>
2023-07-07 18:08:26 +0200

    Update Book.java

--- Book.java
+++ Book.java
@@ -19,14 +19,6 @@ public class Book {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price; 
-    }
-        
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public String getCategory() {
         return category;
